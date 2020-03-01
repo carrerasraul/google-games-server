20fdca9641dc6eb59187fb16c9d432e6517e5441
Raul Carreras <raul.e.carreras@gmail.com>
2020-03-01 18:31:24 -0500

    second commit

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const app = express();
 
 app.use(morgan('common'));
 
 const games = require('./google-games.js')
 
-
 app.get('/games', (req, res) => {
-    // code goes here bruh
+    const { search = "" } = req.query;
+
+    let results = games
+        .filter(games =>
+            games
+            );
+
+    res.json(results);
 })
-
-res
-    .json(games)
-
 app.listen(8000, () =>  {
     console.log('Server started on PORT 8000');
 });
